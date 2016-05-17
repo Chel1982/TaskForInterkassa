13f0c696026b470481165225f1e10951b08084e4
Chel21 <Chel@activist.com>
2016-05-17 17:47:01 +0300

    Intercassa

--- Task1/Task1-Inheritance.php
+++ Task1/Task1-Inheritance.php
@@ -9,21 +9,21 @@ class TextBlock implements Block
 {
     public function render()
     {
-       require_once ("views/Text.html");
+       require_once("views/Text.html");
     }
 }
 class ImageBlock implements Block
 {
     public function render()
     {
-        require_once ("views/Image.html");
+        require_once("views/Image.html");
     }
 }
 class ButtonBlock implements Block
 {
     public function render()
     {
-        require_once ("views/Button.html");
+        require_once("views/Button.html");
     }
 }
 
