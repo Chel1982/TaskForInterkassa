7b1fa62779f1ba42ef86d895f0ab168d87d5075f
Chel21 <Chel@activist.com>
2016-05-18 00:46:29 +0300

    Interkassa

--- Task2/Decorator.php
+++ Task2/Decorator.php
@@ -1,7 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Chel
- * Date: 17.05.2016
- * Time: 23:56
- */
+
+abstract class Block
+{
+    public abstract function render();
+}
+
+class TextBlock extends Block
+{
+    public function render()
+    {
+        require_once("views/Text.html");
+    }
+}
+
+abstract class CBlock extends Block
+{
+    private $text;
+
+    public function __construct(Block $text)
+    {
+        $this -> text = $text;
+    }
+    public function render()
+    {
+        $this -> text -> render();
+    }
+}
+
+class CommentDecorator extends CBlock
+{
+    public function render()
+    {
+        echo <<<HERE
+        <!-- Block BEGIN. Type: <?= get_class($block) ?>, ID: <?= spl_object_hash($block) ?>, Length: <?= $block->getLength() ?> -->
+HERE;
+        parent::render();
+        echo <<<HERE
+        <!-- Block END. Type: <?= get_class($block) ?>, ID: <?= spl_object_hash($block) ?> -->
+HERE;
+
+    }
+}
