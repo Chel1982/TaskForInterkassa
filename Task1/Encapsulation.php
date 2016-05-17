2627658dacf57b32d4eb39442aa70957e4744838
Chel21 <Chel@activist.com>
2016-05-17 21:33:53 +0300

    Interkassa

---
+++ Task1/Encapsulation.php
@@ -0,0 +1,54 @@
+<?php
+
+
+class Block
+{
+    protected $text;
+
+    public function __construct($text)
+    {
+        $this -> text = $text;
+    }
+}
+
+class TextBlock extends Block
+{
+    public function render()
+    {
+        require_once("views/Text.html");
+    }
+    public function adminSay()
+    {
+        echo $this -> text."<br>";
+    }
+}
+
+class ImageBlock extends Block
+{
+    public function render()
+    {
+        require_once("views/Image.html");
+    }
+
+    public function adminSay()
+    {
+        echo $this -> text."<br>";
+    }
+}
+
+class ButtonBlock extends Block
+{
+    public function render()
+    {
+        require_once("views/Button.html");
+    }
+
+    public function adminSay()
+    {
+        echo $this -> text."<br>";
+    }
+}
+
+$text = new ButtonBlock("Hello, This's admin!!");
+$text -> adminSay();
+$text -> render();
