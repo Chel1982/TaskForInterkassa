6ad504123f3c097f9d7c979708799ff577e0a97b
Chel21 <Chel@activist.com>
2016-05-17 17:58:12 +0300

    Intercassa

---
+++ Task1/Inheritance.php
@@ -0,0 +1,31 @@
+<?php
+
+interface Block
+{
+    function Render();
+}
+
+class TextBlock implements Block
+{
+    public function render()
+    {
+       require_once("views/Text.html");
+    }
+}
+class ImageBlock implements Block
+{
+    public function render()
+    {
+        require_once("views/Image.html");
+    }
+}
+class ButtonBlock implements Block
+{
+    public function render()
+    {
+        require_once("views/Button.html");
+    }
+}
+
+$Text = new ImageBlock();
+$Text -> render();
