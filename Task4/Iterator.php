d9384310c187a4f2d0710b443efde2cd7435901a
Chel21 <Chel@activist.com>
2016-06-08 20:36:44 +0300

    Interkassa

--- Task4/Iterator.php
+++ Task4/Iterator.php
@@ -1,2 +1,47 @@
+<pre>
 <?php
 
+class MyIterator implements Iterator
+{
+    private $arr = array();
+
+    public function __construct(Array $array)
+    {
+        $this -> arr = $array;
+    }
+    public function rewind() {
+
+        reset($this->arr);
+    }
+
+    public function current() {
+        $arr = current($this->arr);
+        return $arr;
+    }
+
+    public function key() {
+        $arr = key($this->arr);
+        return $arr;
+    }
+
+    public function next() {
+        $arr = next($this->arr);
+        return $arr;
+    }
+
+    public function valid() {
+        $arr = $this->current() !== false;
+        return $arr;
+    }
+}
+
+$arr = array(
+    "name" => "John",
+    "age" => 25,
+    "admin" => true
+);
+$it = new MyIterator($arr);
+
+foreach ($it as $a => $b) {
+    print "Ключ: $a - Значение: $b\n";
+}
