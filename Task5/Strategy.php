bb56a0c36d9494c716cd21ed1cf676d71f3d4737
Chel21 <Chel@activist.com>
2016-06-09 21:12:38 +0300

    Interkassa

--- Task5/Strategy.php
+++ Task5/Strategy.php
@@ -1,1 +1,26 @@
 <?php
+
+abstract class Comment
+{
+    protected abstract function ComentBlock();
+}
+
+class CommentUp extends Comment
+{
+    function ComentBlock()
+    {
+        echo "&lt!-- Block BEGIN. Class:".__CLASS__." -->";}
+    }
+
+class CommentDown extends Comment
+{
+    function ComentBlock()
+    {
+        echo "&lt!-- Block END. Class:".__CLASS__." -->";
+    }
+}
+
+class StrategyComment
+{
+
+}
